2c7856a6aebdc91d54042cebb9857d25b432d2d3
KyotoAnime <30362911+KyotoAnime@users.noreply.github.com>
2018-09-15 13:11:21 +0800

    x

--- main.py
+++ main.py
@@ -159,6 +159,133 @@ def train(**kwargs):
     
     return
 
+def train_x(**kwargs):
+    
+    opt = DefaultConfig()
+    opt.parse(kwargs)
+    
+    dataset = StyleAB(opt.data_root, train=True)
+    train_dataloader = DataLoader(dataset, opt.batch_size, shuffle=True)
+    dataset = StyleAB(opt.data_root, train=False)
+    test_dataloader = DataLoader(dataset, opt.batch_size, shuffle=False)
+       
+    G_A2B = Generator()
+    G_B2A = Generator()
+    D = Discriminator()
+    
+    while True:
+        
+      try:
+      
+        G_A2B = G_A2B.cuda()
+        G_B2A = G_B2A.cuda()
+        D = D.cuda()
+        break
+        
+      except:
+        
+        print('Fail ',time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+        time.sleep(60)
+    
+    criterion_real = nn.MSELoss()
+    criterion_recons = nn.L1Loss()
+    
+    optimizer_G = t.optim.Adam(itertools.chain(G_A2B.parameters(), G_B2A.parameters()), 
+                               lr=opt.lr, betas=(0.5, 0.999))
+    optimizer_D = t.optim.Adam(D.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    
+    vis = Visualizer(opt.env)
+    vis.add_names('loss_G', 'loss_D')
+    
+    for epoch in range(opt.n_epoch):
+        
+        avgloss_G = 0
+        avgloss_D = 0
+        
+        for i, (real_A, real_B, img_names_A, img_names_B) in enumerate(train_dataloader):
+            
+            real_A = real_A.cuda()
+            real_B = real_B.cuda()
+            
+            label_B = t.ones(real_A.size()[0], 1).cuda()
+            label_A = t.zeros(real_A.size()[0], 1).cuda()
+            
+            gener_B = G_A2B(real_A)
+            recons_A = G_B2A(gener_B)
+    
+            gener_A = G_B2A(real_B)
+            recons_B = G_A2B(gener_A)
+            
+            loss_G1 = criterion_real(D(gener_B), label_B) + criterion_recons(recons_A, real_A)*opt.lambda_cyle
+            loss_G2 = criterion_real(D(gener_A), label_A) + criterion_recons(recons_B, real_B)*opt.lambda_cyle
+            loss_G = loss_G1 + loss_G2
+            
+            optimizer_G.zero_grad()
+            loss_G.backward()
+            optimizer_G.step()
+            
+            gener_A = gener_A.detach()
+            gener_B = gener_B.detach()
+            
+            loss_D_A = criterion_real(D(gener_A), label_B) + criterion_real(D(real_A), label_A)
+            loss_D_A = loss_D_A*0.5
+            
+            loss_D_B = criterion_real(D(gener_B), label_A) + criterion_real(D(real_B), label_B)
+            loss_D_B = loss_D_B*0.5
+            
+            loss_D = loss_D_A + loss_D_B
+            
+            optimizer_D.zero_grad()
+            loss_D.backward()
+            optimizer_D.step()
+            
+            avgloss_G += loss_G.cpu().item()
+            avgloss_D += loss_D.cpu().item()
+        
+        vis.plot('loss_G', epoch, avgloss_G/i)
+        vis.plot('loss_D', epoch, avgloss_D/i)
+        
+        test_iter = iter(test_dataloader)
+        real_A, real_B, img_names_A, img_names_B = next(test_iter)
+        
+        real_A = real_A.cuda()
+        real_B = real_B.cuda()
+
+        gener_B = G_A2B(real_A)
+        gener_A = G_B2A(real_B)
+        
+        real_A = real_A*0.5 + 0.5 
+        real_B = real_B*0.5 + 0.5
+        gener_A = gener_A*0.5 + 0.5
+        gener_B = gener_B*0.5 + 0.5
+        
+        real_A = real_A.cpu()
+        real_B = real_B.cpu()
+        gener_A = gener_A.cpu()
+        gener_B = gener_B.cpu()
+        
+        vis.imgs('real_A', real_A)
+        vis.imgs('real_B', real_B)
+        vis.imgs('gener_A', gener_A)
+        vis.imgs('gener_B', gener_B)
+        
+        vis.log('epoch:{}, loss_G:{}, loss_D:{}'.format(epoch, avgloss_G/i, avgloss_D/i))
+        
+        if not os.path.isdir('checkpoints'):
+            os.mkdir('checkpoints')
+            
+        t.save(G_A2B.state_dict(), 'checkpoints/G_A2B_e{}.ckpt'.format(epoch+1))
+        t.save(G_B2A.state_dict(), 'checkpoints/G_B2A_e{}.ckpt'.format(epoch+1))
+        
+        if epoch > 100:
+            opt.lr -= 0.0002*0.01
+            
+        optimizer_G = t.optim.Adam(itertools.chain(G_A2B.parameters(), G_B2A.parameters()), 
+                                   lr=opt.lr, betas=(0.5, 0.999))
+        optimizer_D = t.optim.Adam(D.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    
+    return
+
 def test(**kwargs):
     
     opt = DefaultConfig()
